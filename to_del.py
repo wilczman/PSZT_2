ba6b90f8f4df1a19643810705e396cf492c22cf6
wilczman <38138814+wilczman@users.noreply.github.com>
2021-01-24 19:38:49 +0100

    2

--- to_del.py
+++ to_del.py
@@ -1,9 +1,12 @@
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
+import numpy
 import mnist
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+limit = 1000
+
 
 digits = datasets.load_digits()
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
@@ -13,17 +16,26 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_title('Training: %i' % label)
 
 # flatten the images
-# n_samples = len(digits.images)
-# data = digits.images.reshape((n_samples, -1))
-# X_train, X_test, y_train, y_test = train_test_split(
-#     data, digits.target, test_size=0.5, shuffle=False)
-# print(y_test)
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+X_train2, X_test2, y_train2, y_test2 = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=False)
+print(X_train2[1])
+print(y_train2[1])
+print(X_test2[1])
+print(y_test2[1])
+
 X_train, y_train, X_test, y_test = mnist.load()
-limit = 1000
-X_train = X_train[1:limit, :]
-X_test = X_test[1:limit, :]
-y_train = y_train[1:limit]
-y_test = y_test[1:limit]
+X_train = X_train[1:limit, :].astype(numpy.float64)/16.0
+X_test = X_test[1:limit, :].astype(numpy.float64)/16.0
+y_train = y_train[1:limit].astype(numpy.float64)
+y_test = y_test[1:limit].astype(numpy.float64)
+
+print(X_train[1])
+print(y_train[1])
+print(X_test[1])
+print(y_test[1])
+
 # Create a classifier: a support vector classifier
 clf = svm.SVC(gamma=0.001)
 
@@ -40,11 +52,11 @@ for ax, image, prediction in zip(axes, X_test, predicted):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     ax.set_title(f'Prediction: {prediction}')
 
-print(f"Classification report for classifier {clf}:\n"
-      f"{metrics.classification_report(y_test, predicted)}\n")
+# print(f"Classification report for classifier {clf}:\n"
+#       f"{metrics.classification_report(y_test, predicted)}\n")
 
 disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
 disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
+# print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
 plt.show()
