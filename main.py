fdf57320600d7fca343eee19d7c240a70fe02f12
wilczman <38138814+wilczman@users.noreply.github.com>
2021-01-24 01:25:15 +0100

    to_del gotowa implementacja
    nie działa

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import mnist
+import numpy
 from sklearn import datasets, svm, metrics
 import matplotlib.pyplot as plt
 import time
 # mnist.init()
 t = time.time()
 x_train, t_train, x_test, t_test = mnist.load()
-
-clf = svm.SVC(gamma=0.001)
-clf.fit(x_train[1:100,:], t_train[1:100])
-predicted = clf.predict(x_test[1:100,:])
-print(predicted)
-print(time.time()-t)
-
+# X_train, X_test, y_train, y_test = mnist.load()
+# X_train = X_train[1:5000, :]
+# X_test = X_test[1:5000, :]
+# y_train = y_train[1:5000]
+# y_test = y_test[1:5000]
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
+print(x_train)
+for i, x in enumerate(x_train[1:10]):
+    x_train_2.append(x.reshape(28, 28))
+# for x in x_train[1:10]:
+#     print(x)
+
+for ax, image in zip(axes, x_test):
     ax.set_axis_off()
+    image = image.reshape(28, 28)
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    ax.set_title('Training: %i' % label)
+    ax.set_title(f'Prediction: none')
+
+plt.show()
+
+# clf = svm.SVC(gamma=0.001)
+# clf.fit(x_train[1:100,:], t_train[1:100])
+# predicted = clf.predict(x_test[1:100,:])
+# print(predicted)
+# print(time.time()-t)
+
+# _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+# for ax, image, label in zip(axes, digits.images, digits.target):
+#     ax.set_axis_off()
+#     ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+#     ax.set_title('Training: %i' % label)
 
 
 # _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
--- to_del.py
+++ to_del.py
@@ -1,6 +1,6 @@
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
-
+import mnist
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
@@ -13,15 +13,19 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_title('Training: %i' % label)
 
 # flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
-
+# n_samples = len(digits.images)
+# data = digits.images.reshape((n_samples, -1))
+# X_train, X_test, y_train, y_test = train_test_split(
+#     data, digits.target, test_size=0.5, shuffle=False)
+# print(y_test)
+X_train, y_train, X_test, y_test = mnist.load()
+limit = 1000
+X_train = X_train[1:limit, :]
+X_test = X_test[1:limit, :]
+y_train = y_train[1:limit]
+y_test = y_test[1:limit]
 # Create a classifier: a support vector classifier
 clf = svm.SVC(gamma=0.001)
-
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False)
 
 # Learn the digits on the train subset
 clf.fit(X_train, y_train)
@@ -32,6 +36,15 @@ predicted = clf.predict(X_test)
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, prediction in zip(axes, X_test, predicted):
     ax.set_axis_off()
-    image = image.reshape(8, 8)
+    image = image.reshape(28, 28)
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     ax.set_title(f'Prediction: {prediction}')
+
+print(f"Classification report for classifier {clf}:\n"
+      f"{metrics.classification_report(y_test, predicted)}\n")
+
+disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
+disp.figure_.suptitle("Confusion Matrix")
+print(f"Confusion matrix:\n{disp.confusion_matrix}")
+
+plt.show()
