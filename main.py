cf4165bc1d17f06fd4de623821cf3db65e596e96
Przemyslaw Kacprowicz <przemyslaw.kacprowicz.stud@pw.edu.pl>
2021-01-27 23:11:02 +0100

    Beutify main

--- main.py
+++ main.py
@@ -16,26 +16,51 @@ y_train = y_train[0:limit_train]
 y_test = y_test[0:limit_test]
 
 if __name__ == '__main__':
+
+    print("Testowanie liniowego klasyfikatora SVM w wersji OVO")
     classifier = svm_pszt.SVM
-    clf = ensembles.OVREnsemble(classifier, rbf_kernel)
-
+    clf = ensembles.OVOEnsemble(classifier)
     clf.fit(X_train, y_train)
-
-    # Predict the value of the digit on the test subset
     predicted = clf.predict(X_test).astype(np.int8)
-
-    _, axes = plt.subplots(nrows=1, ncols=12, figsize=(15, 5))
-    for ax, image, prediction in zip(axes, X_test, predicted):
-        ax.set_axis_off()
-        image = image.reshape(28, 28)
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-        ax.set_title(f'Prediction: {prediction}')
-
-
-    plt.show()
-
     confusiomMatrix = metrics.confusion_matrix(y_test, predicted)
     disp = metrics.ConfusionMatrixDisplay(confusiomMatrix, display_labels=np.unique(y_test))
     disp.plot()
-    print(np.sum(np.ones(y_test.shape)[(y_test == predicted)]) / len(y_test))
+    correct_rate = np.sum(np.ones(y_test.shape)[(y_test == predicted)]) / len(y_test)
+    print(f"Procent porawnych klasyfikacji lin SVM OVO: {correct_rate * 100}%")
+
+
+    print("Testowanie liniowego klasyfikatora SVM w wersji OVR")
+    classifier = svm_pszt.SVM
+    clf = ensembles.OVREnsemble(classifier)
+    clf.fit(X_train, y_train)
+    predicted = clf.predict(X_test).astype(np.int8)
+    confusiomMatrix = metrics.confusion_matrix(y_test, predicted)
+    disp = metrics.ConfusionMatrixDisplay(confusiomMatrix, display_labels=np.unique(y_test))
+    disp.plot()
+    correct_rate = np.sum(np.ones(y_test.shape)[(y_test == predicted)]) / len(y_test)
+    print(f"Procent porawnych klasyfikacji lin SVM OVR: {correct_rate * 100}%")
+
+
+    print("Testowanie klasyfikatora SVM z wykorzystaniem kernela RBF w wersji OVO")
+    classifier = svm_pszt.SVM
+    clf = ensembles.OVOEnsemble(classifier, rbf_kernel)
+    clf.fit(X_train, y_train)
+    predicted = clf.predict(X_test).astype(np.int8)
+    confusiomMatrix = metrics.confusion_matrix(y_test, predicted)
+    disp = metrics.ConfusionMatrixDisplay(confusiomMatrix, display_labels=np.unique(y_test))
+    disp.plot()
+    correct_rate = np.sum(np.ones(y_test.shape)[(y_test == predicted)]) / len(y_test)
+    print(f"Procent porawnych klasyfikacji RBF SVM OVO: {correct_rate * 100}%")
+
+
+    print("Testowanie klasyfikatora SVM z wykorzystaniem kernela RBF w wersji OVR")
+    classifier = svm_pszt.SVM
+    clf = ensembles.OVREnsemble(classifier, rbf_kernel)
+    clf.fit(X_train, y_train)
+    predicted = clf.predict(X_test).astype(np.int8)
+    confusiomMatrix = metrics.confusion_matrix(y_test, predicted)
+    disp = metrics.ConfusionMatrixDisplay(confusiomMatrix, display_labels=np.unique(y_test))
+    disp.plot()
+    correct_rate = np.sum(np.ones(y_test.shape)[(y_test == predicted)]) / len(y_test)
+    print(f"Procent porawnych klasyfikacji RBF SVM OVR: {correct_rate * 100}%")
     
