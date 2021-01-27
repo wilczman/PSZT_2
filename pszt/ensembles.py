d9e227c9646024bb413f6ea315bb929b815dc20f
Przemyslaw Kacprowicz <przemyslaw.kacprowicz.stud@pw.edu.pl>
2021-01-27 20:16:32 +0100

    OVR

--- pszt/ensembles.py
+++ pszt/ensembles.py
@@ -57,6 +57,61 @@ class OVOEnsemble(object):
         return prediction
 
 
+class OVREnsemble(object):
+
+    def __init__(self, classifier):
+        self._classifier = classifier
+        
+
+    def fit(self, X, y):
+        self._labels = np.unique(y)
+        self._scaler = np.max(X)
+        self._X_train = X / self._scaler
+        self._y_train = y
+
+        self._models = [None] * self._labels.shape[0]
+
+        for i, pClass in enumerate(self._labels):
+            X = self._X_train
+            y = self._y_train.astype(np.double)
+
+            positive_indices = (y == pClass)
+            negative_indices = (y != pClass)
+            y[positive_indices] = np.ones(sum(positive_indices)).reshape(-1)
+            y[negative_indices] = -np.ones(sum(negative_indices)).reshape(-1)
+
+            self._models[i] = self._classifier()
+            self._models[i].fit(X, y)
+
+    def predict(self, X_in):
+        X = X_in / self._scaler
+        votes = np.zeros((X.shape[0], self._labels.shape[0]))
+
+        for label_idx, model in enumerate(self._models):
+
+            result = model.predict(X)
+
+            positive_indices = (result == 1)
+            negative_indices = (result == -1)
+
+            result_labels = np.zeros(result.shape)
+            result_labels[positive_indices] = np.repeat(self._labels[label_idx], sum(positive_indices))
+            result_labels[negative_indices] = np.repeat(self._labels.shape[0], sum(negative_indices))
+
+            for idx_sample, result in enumerate(result_labels):
+                idx_label = np.where(result == self._labels)
+                if idx_label == self._labels.shape[0]:
+                    continue
+                votes[idx_sample, idx_label] += 1
+
+        final_idx = np.argmax(votes, axis=1)
+
+        prediction = np.zeros(X.shape[0])
+        for i, label_idx in enumerate(final_idx):
+            prediction[i] = self._labels[label_idx]
+            
+        return prediction
+
 
             
 
